9697daf388bcaf845853c6ba33eb6bb5bd0f086b
Daniel Singian <daniel.singian02@gmail.com>
2022-11-11 15:50:52 +0800

    Add styling to the Search page

--- header.php
+++ header.php
@@ -14,53 +14,55 @@
 
 <body>
 
-	<header>
-		<nav>
-			<div class="brand">
-				<a class="navbar-brand" href="<?php bloginfo('url'); ?>">
-					<?php
+	<section>
+		<header>
+			<nav>
+				<div class="brand">
+					<a class="navbar-brand" href="<?php bloginfo('url'); ?>">
+						<?php
 
-					$custom_logo_id = get_theme_mod('custom_logo');
-					$logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+						$custom_logo_id = get_theme_mod('custom_logo');
+						$logo = wp_get_attachment_image_src($custom_logo_id, 'full');
 
-					if (has_custom_logo()) {
-						echo '<img class="header_logo" src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
-					} else {
-						echo '<h1>' . get_bloginfo('name') . '</h1>';
-					}
+						if (has_custom_logo()) {
+							echo '<img class="header_logo" src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
+						} else {
+							echo '<h1>' . get_bloginfo('name') . '</h1>';
+						}
 
-					?>
-				</a>
-			</div>
-			<div class="nav-links">
-				<div class="menu-container">
-					<?php wp_nav_menu('primary'); ?>
-					<div class="user-portal">
-						<span class="user-portal__links">
-							<a href="#">Login</a>
-							<a href="#">Register</a>
-						</span>
-					</div>
+						?>
+					</a>
 				</div>
-
-				<button class="btn hamburger" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-					<i class="fa-solid fa-bars"></i>
-				</button>
-
-				<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-					<div class="offcanvas-header">
-						<h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-						<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+				<div class="nav-links">
+					<div class="menu-container">
+						<?php wp_nav_menu('primary'); ?>
+						<div class="user-portal">
+							<span class="user-portal__links">
+								<a href="#">Login</a>
+								<a href="#">Register</a>
+							</span>
+						</div>
 					</div>
-					<div class="offcanvas-body">
-						<div class="menu-container-responsive text-black">
-							<?php wp_nav_menu('primary'); ?>
-							<ul>
-								<li>Login | Register</li>
-							</ul>
+
+					<button class="btn hamburger" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+						<i class="fa-solid fa-bars"></i>
+					</button>
+
+					<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+						<div class="offcanvas-header">
+							<h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+							<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+						</div>
+						<div class="offcanvas-body">
+							<div class="menu-container-responsive text-black">
+								<?php wp_nav_menu('primary'); ?>
+								<ul>
+									<li>Login | Register</li>
+								</ul>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</nav>
-	</header>
+			</nav>
+		</header>
+	</section>
--- search.php
+++ search.php
@@ -1,52 +1,58 @@
 <?php
-
 get_header(); ?>
-  <div class="site-content-search">
-	  <header class="page-header-search">
-		  <h1 class="page-title-search">
-			  <?php
-				  /* translators: %s: the search query */
-				  printf( esc_html__( 'Search Results for: %s', 'scaffold' ), '<span>' . get_search_query() . '</span>' );
-			  ?>
-		  </h1>
-	  </header><!-- .page-header -->
-	    <?php
-	    if ( have_posts() ) :
 
-	      while ( have_posts() ) :
+<div class="site-content-search">
+	<div class="search-results">
+		<header class="page-header-search">
+			<h1 class="page-title-search">
+				<?php
+					/* translators: %s: the search query */
+					printf( esc_html__( 'Search Results for: %s', 'scaffold' ), '<span>' . get_search_query() . '</span>' );
+				?>
+			</h1>
+		</header><!-- .page-header -->
+		<?php
+		if ( have_posts() ) :
 
-	        the_post();
-	        ?>
+			while ( have_posts() ) :
 
-	        <article <?php post_class(); ?>>
+				the_post();
+				?>
 
-			  <?php the_post_thumbnail( 'my-custom-image-size' ); ?>
+				<article <?php post_class(); ?>>
+					<a href="<?php echo esc_url( get_permalink() ); ?>" target="_blank" >
 
-	          <header class="entry-header-search">
-	            <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-	          </header><!-- .entry-header -->
+						<div class="content">
+							<?php the_post_thumbnail( 'my-custom-image-size' ); ?>
 
-	          <div class="entry-content-search">
-	            <?php the_content( esc_html__( 'Continue reading &rarr;', 'my-custom-theme' ) ); ?>
-	          </div><!-- .entry-content -->
+							<header class="entry-header-search">
+								<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+							</header><!-- .entry-header -->
 
-	        </article><!-- #post-## -->
+							<div class="entry-content-search">
+								<?php the_content( esc_html__( 'Continue reading &rarr;', 'my-custom-theme' ) ); ?>
+							</div><!-- .entry-content -->
+						</div>
+					</a>
+				</article><!-- #post-## -->
 
-	        <?php
-	        // If comments are open or we have at least one comment, load up the comment template.
-	        if ( comments_open() || get_comments_number() ) :
-	          comments_template();
-	        endif;
+				<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-	      endwhile;
+			endwhile;
 
-		  the_posts_navigation();
+			the_posts_navigation();
 
-	    else :
-	      get_template_part( 'content-none' );
-	    endif;
-		?>
-		</div><!-- .site-content -->
-        <?php
+			else :
+				get_template_part( 'content-none' );
+		endif;
+			?>
+	</div>
+</div><!-- .site-content -->
+
+<?php
 get_footer();
 ?>
